ef69fff789005ccd0b00866bfbfe325547de9508
Daniel Mateos <daniel@mateos.cc>
2015-12-01 22:43:09 +1100

    tests

--- board.js
+++ board.js
@@ -1,12 +1,22 @@
 Board = function(container) {
+  var x_max = 24;
+  var y_max = 24;
   var container = container;
   var hexes = new Array();
 
-  var build_hexgrid = function() {
-    for(var x = 0; x < 12; x++) {
+  PIXI.loader
+    .add("waterHex", "assets/waterHex.gif")
+    .add("woodHex", "assets/clayHex.gif")
+    .load(function(loader, resources) {
+      build_hexgrid(resources);
+      compute_hexgrid_neighbours();
+    });
+
+  var build_hexgrid = function(resources) {
+    for(var x = 0; x < x_max; x++) {
       var line = new Array();
-      for(var y = 0; y < 12; y++) {
-        h = new Hex(x, y);
+      for(var y = 0; y < y_max; y++) {
+        var h = new Hex(x, y, resources.woodHex.texture);
         line.push(h);
         container.addChild(h.get_sprite());
       }
@@ -15,20 +25,41 @@ Board = function(container) {
   }
 
   var compute_hexgrid_neighbours = function() {
-    for(var x = 0; x < 12; x++) {
-      for(var y = 0; y < 12; y++) {
+    for(var x = 0; x < x_max; x++) {
+      for(var y = 0; y < y_max; y++) {
         h = hexes[x][y];
         h.set_neighbour("north", hexes[x][y-1]);
         h.set_neighbour("south", hexes[x][y+1]);
-        if(typeof(hexes[x-1]) != "undefined" && typeof(hexes[x-1][y+1] != "undefined")) { 
-          h.set_neighbour("south_west", hexes[x-1][y+1]);
+        if(x % 2 != 0) {
+          if(typeof(hexes[x-1]) != "undefined" && typeof(hexes[x-1][y+1]) != "undefined") { 
+            h.set_neighbour("south_west", hexes[x-1][y+1]);
+          }
+          if(typeof(hexes[x-1]) != "undefined" && typeof(hexes[x-1][y]) != "undefined") {
+            h.set_neighbour("north_west", hexes[x-1][y]);
+          }
+          if(typeof(hexes[x+1]) != "undefined" && typeof(hexes[x+1][y]) != "undefined") {
+            h.set_neighbour("north_east", hexes[x+1][y]); 
+          }
+          if(typeof(hexes[x+1]) != "undefined" && typeof(hexes[x+1][y+1]) != "undefined") {
+            h.set_neighbour("south_east", hexes[x+1][y+1]);
+          }
+        } else {
+          if(typeof(hexes[x-1]) != "undefined" && typeof(hexes[x-1][y+2]) != "undefined") { 
+            h.set_neighbour("south_west", hexes[x-1][y]);
+          }
+          if(typeof(hexes[x-1]) != "undefined" && typeof(hexes[x-1][y-1]) != "undefined") {
+            h.set_neighbour("north_west", hexes[x-1][y-1]);
+          }
+          if(typeof(hexes[x+1]) != "undefined" && typeof(hexes[x+1][y-1]) != "undefined") {
+            h.set_neighbour("north_east", hexes[x+1][y-1]);
+          }
+          if(typeof(hexes[x+1]) != "undefined" && typeof(hexes[x+1][y]) != "undefined") {
+            h.set_neighbour("south_east", hexes[x+1][y]); 
+          }
         }
       }
     }
   }
-
-  build_hexgrid();
-  compute_hexgrid_neighbours();
 
   this.render = function(renderer) {
      renderer.render(container);
--- hex.js
+++ hex.js
@@ -1,6 +1,4 @@
-Hex = function(xp, yp) {
-  var texture = PIXI.Texture.fromImage("assets/hex.png");
-  var texture_green = PIXI.Texture.fromImage("assets/hex_green.png");
+Hex = function(xp, yp, texture) {
   var sprite = new PIXI.Sprite(texture);
 
   var x = xp;
@@ -13,16 +11,19 @@ Hex = function(xp, yp) {
   var south_east;
   var south_west;
 
+  var hex_height = texture.height;
+  var hex_width = texture.width;
+
   sprite.anchor.x = 0;
   sprite.anchor.y = 0;
   sprite.interactive = true;
 
   if(x % 2 == 0) {
-    sprite.position.x = (x * 128) - (x * 35);
-    sprite.position.y = (y * 107);
+    sprite.position.x = (x * hex_width) - (x * (hex_width/4));
+    sprite.position.y = (y * hex_height);
   } else {
-    sprite.position.x = (x * 128) - (x * 35);
-    sprite.position.y = (y * 107) + 50;
+    sprite.position.x = (x * hex_width) - (x * (hex_width/4));
+    sprite.position.y = (y * hex_height) + (hex_height/2 );
   }
 
   this.get_sprite = function() {
@@ -31,12 +32,12 @@ Hex = function(xp, yp) {
 
   this.set_green = function() {
     sprite.texture = texture_green;
-  }
+    }
 
   this.set_normal = function() {
     sprite.texture = texture;
   }
-
+  
   this.set_neighbour = function(neighbour_direction, neighbour) {
     switch (neighbour_direction) {
       case "north":
@@ -60,11 +61,31 @@ Hex = function(xp, yp) {
     }
   }
 
-  sprite.mouseover = function(mouseData) {
+  sprite.click = function(mouseData) {
     console.log(x + " " + y);
 
     sprite.texture = texture_green;
-    //south.set_green();
-    //south_west.set_green();
+    if(south_west)
+    south_west.set_green();
+
+    if(north_west) {
+      north_west.set_green();
+    }
+
+    if(north_east) {
+      north_east.set_green();
+    }
+
+    if(south_east) {
+      south_east.set_green();
+    }
+
+    if(north) {
+      north.set_green();
+    }
+
+    if(south) {
+      south.set_green();
+    }
   }
 }
